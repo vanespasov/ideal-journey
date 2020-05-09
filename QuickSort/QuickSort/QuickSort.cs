4918b6377a24ccb6a9760cb40a0f4ad9a70f5442
Vane Spasov <Vane.Spasov@endava.com>
2020-05-09 19:44:30 +0200

    VS: added Visual Studio project QuickSort implemented in C#.

---
+++ QuickSort/QuickSort/QuickSort.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    public class QuickSort
+    {
+        int[] _array;
+        public QuickSort(int[] array)
+        {
+            _array = array;
+        }
+
+        private int Partition(int[] array, int low, int high)
+        {
+            //1. Select a pivot point.
+            int pivot = array[high];
+
+            int lowIndex = (low - 1);
+
+            //2. Reorder the collection.
+            for (int j = low; j < high; j++)
+            {
+                if (array[j] <= pivot)
+                {
+                    lowIndex++;
+
+                    int temp = array[lowIndex];
+                    array[lowIndex] = array[j];
+                    array[j] = temp;
+                }
+            }
+
+            int temp1 = array[lowIndex + 1];
+            array[lowIndex + 1] = array[high];
+            array[high] = temp1;
+
+            return lowIndex + 1;
+        }
+
+        private void Sort(int[] array, int low, int high)
+        {
+            if (low < high)
+            {
+                int partitionIndex = Partition(array, low, high);
+
+                //3. Recursively continue sorting the array
+                Sort(array, low, partitionIndex - 1);
+                Sort(array, partitionIndex + 1, high);
+            }
+        }
+
+        private void Print(int[] array)
+        {
+            int length = array.Length;
+            for (int i = 0; i < length; ++i)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        #region Public methods
+        public void Sort(int low, int high)
+        {
+            Sort(_array, low, high);
+        }
+
+        public void PrintInitial(int[] array)
+        {
+            Console.Write("Initial array is: ");
+            Print(array);
+        }
+        public void PrintFinal(int[] array)
+        {
+            Console.Write("Sorted array is: ");
+            Print(array);
+        }
+        #endregion
+    }
+}
